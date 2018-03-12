baab0cce3d01fa07e7e8e5735548b5c34b5ee6b9
Yury Staravoitau <MorfinAll@mail.ru>
2018-03-13 00:36:35 +0300

    Implement 1 test with numerable property

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
 // DO WHATEVER YOU WANT HERE
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+const createEnumerableProperty = (propertyName) => {
+    return propertyName
+};
+
+const createNotEnumerableProperty = (propertyName) => {
+};
+const createProtoMagicObject = () => {
+
+};
+
+const incrementor = () => {
+    // let increment = 0;
+    // let counter = function (){
+    //     increment++;
+    //     return counter ;
+    // }
+
+    // counter.valueOf ()= function (){
+    //     return increment;
+    // }
+
+    // return counter;
+};
+const asyncIncrementor = () => { };
+const createIncrementer = () => { };
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
-const toBuffer = () => {};
-const sortByProto = () => {};
+const returnBackInSecond = () => { };
+const getDeepPropertiesCount = () => { };
+const createSerializedObject = () => { };
+const toBuffer = () => { };
+const sortByProto = () => { };
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
